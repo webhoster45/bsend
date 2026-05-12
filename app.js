331a14ebd65e5905017fe553f0fa449b2c7cba53
webhoster45 <gbengaayoolawale@gmail.com>
2026-05-12 22:26:52 +0100

    Finished authmiddleware code

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const app=express();
 const MONGODB_URL=process.env.MONGODB_URL
 const path=require('path');
 const Schema=mongoose.Schema;
-const limiter=require('limiter');
+const limiter=require('express-rate-limiter');
 const jwt=require('jsonwebtoken');
 const PORT=process.env.PORT||3000;
+const bcrypt=require('bcrypt')
+const JWT_SECRET=process.env.JWT_SECRET
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('/public'))
 
 
 mongoose.connect(MONGODB_URL)
@@ -14,16 +19,32 @@ mongoose.connect(MONGODB_URL)
 ()=>{
     console.log("Mongodb connected");
 app.listen(PORT,console.log(`App running on ${PORT}}`))})
-.catch((err)=>console.log(err))
+.catch((err)=>console.log(err));
 
-const authmiddleware=(req,res)=>{
-const token=req.headers["authorization"] [1]
-if(!token) res.status(403)
+const limiter=()=>{
+    Windowms:15*60;
+    limit:5
+}
+const authmiddleware=(req,res,next)=>{
+const authheader=req.headers.authorization;
+if(authheader)return res.status(401)
+if(!token) res.status(403).json({message:"No token provided"});
+const token=authheader.split(" ")[1];
+jwt.verify(token, JWT_SECRET, (err,decoded)=>{
+    if(err)return res.status(403).json({message:"Invalid or Expired token"});
+    req.user=decoded;
+    next()
+})
 }
 
-app.post('/register',(req,res))
+app.post('/register',(req,res)=>{
+    const {username , password}
+})
 app.post('/login',(req,res))
 
 app.use((req,res)=>{
     res.status(404).json({message:"This route doesn't exist"})
 })
+
+
+//install bycrpt express-rate-limiter 
